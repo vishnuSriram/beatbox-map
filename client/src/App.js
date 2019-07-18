67e03f3d82142b368ef55a53322dfea14d9e52c1
Vishnu Sriram <vsriram42@gmail.com>
2019-07-17 22:38:44 -0500

    Front end edits

--- client/src/App.js
+++ client/src/App.js
@@ -22,40 +22,8 @@ class App extends Component {
 
     return (
       <div className="App">
-        {/* Render the passwords if we have them */}
-        {passwords.length ? (
-          <div>
-            <h1>5 Passwords.</h1>
-            <ul className="passwords">
-              {/*
-                Generally it's bad to use "index" as a key.
-                It's ok for this example because there will always
-                be the same number of passwords, and they never
-                change positions in the array.
-              */}
-              {passwords.map((password, index) =>
-                <li key={index}>
-                  {password}
-                </li>
-              )}
-            </ul>
-            <button
-              className="more"
-              onClick={this.getPasswords}>
-              Get More
-            </button>
-          </div>
-        ) : (
-          // Render a helpful message otherwise
-          <div>
-            <h1>No passwords :(</h1>
-            <button
-              className="more"
-              onClick={this.getPasswords}>
-              Try Again?
-            </button>
-          </div>
-        )}
+        <h1>Welcome to Beatbox Map!</h1>
+        <p>Written by: Vishnu, Shak, and Abdulla</p>
       </div>
     );
   }
