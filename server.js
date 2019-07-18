ca88cf84b5c80b497083e76ef53ce9b90b9af3fc
Vishnu Sriram <vsriram42@gmail.com>
2019-07-17 19:52:21 -0500

    Added routes to test server in Heroku

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+const db = require('./queries')
+
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
+
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(__dirname + '/public'));
+
+// set the home page route
+app.get('/', function(req, res) {
+    // React renders automatically 
+    res.render('index');
+});
+
+app.get('/statemode', db.getStateMode);
+
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
+});
+
