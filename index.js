049fe32415ea2fc34bc76aa1cc137587e9a5d0d7
Vishnu Sriram <vsriram42@gmail.com>
2019-07-18 22:32:11 -0500

    Rewrote get request and database connect

--- index.js
+++ index.js
@@ -1,14 +1,44 @@
 const express = require('express');
-const path = require('path');
-const db = require('./queries');
 const app = express();
+const path = require('path');
+
+// Required to connect to Heroku Postgres
+const { Pool, Client } = require('pg')
+const connectionString = 'postgres://vaafvneyyzbmoe:1a5492aecf2dc8ee29c5ba32e4e0d3778d32e63eb7d723ca4e238ceacf166c5b@ec2-23-21-115-109.compute-1.amazonaws.com:5432/d3rvg9a7cpnip'
+const pool = new Pool({
+  connectionString: connectionString,
+  ssl: true
+})
+
+const client = new Client({
+  connectionString: connectionString,
+  ssl: true
+})
+client.connect()
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 // Put all API endpoints under '/api'
-app.get('/api/beatboxers', db.getAllBeatboxers);
-
+app.get('/api/beatboxers', (req, res) => {
+  pool.connect((err, client, done) => {
+      const query = 'SELECT * FROM beatboxers';
+      client.query(query, (error, result) => {
+        done();
+        if (error) {
+          res.status(400).json({error})
+        } 
+        if(result.rows < '1') {
+          res.status(404).send({
+          status: 'Failed',
+          message: 'No beatboxer information found',
+          });
+        } else {
+          res.status(200).send(result.rows);
+        }
+      });
+    });
+  });
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('*', (req, res) => {
@@ -16,6 +46,7 @@ app.get('*', (req, res) => {
 });
 
 const port = process.env.PORT || 5000;
-app.listen(port);
 
-console.log(`Beatbox Map listening on ${port}`);
+app.listen(port, () => {
+  console.log(`Beatbox Map is live at 127.0.0.1:${port}`);
+});
--- queries.js
+++ queries.js
@@ -1,30 +1,43 @@
-const { Client } = require('pg');
-
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  dialectOption: {
-    ssl: true
-  }
-});
-
-client.connect();
+// IMPORTANT
+// Code required to connect to Heroku DB
 
 /*
-client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-  if (err) throw err;
-  for (let row of res.rows) {
-    console.log(JSON.stringify(row));
-  }
-  client.end();
-});
-*/
+const { Pool, Client } = require('pg')
+const connectionString = 'postgres://vaafvneyyzbmoe:1a5492aecf2dc8ee29c5ba32e4e0d3778d32e63eb7d723ca4e238ceacf166c5b@ec2-23-21-115-109.compute-1.amazonaws.com:5432/d3rvg9a7cpnip'
+const pool = new Pool({
+  connectionString: connectionString,
+  ssl: true
+})
+pool.query('SELECT NOW()', (err, res) => {
+  console.log(err, res)
+  pool.end()
+})
+const client = new Client({
+  connectionString: connectionString,
+  ssl: true
+})
+client.connect()
 
+*/
+const getAllBeatboxers = (request, response) => {
+  client.query('SELECT * FROM beatboxers', (err, res) => {
+    console.log(err, res)
+    client.end()
+  })
+}
+
+// Exports all query results
+module.exports = {
+  getAllBeatboxers
+}
+
+/*
 // Find most common city for a beatboxer
 const getCityMode = (request, response) => {
     client.query('SELECT MODE() WITHIN GROUP (ORDER BY city) AS city_mode FROM beatboxers;', function(err, res) {
       if (err) throw err;
         for (let row of res.rows) {
-          response.status(200).json(res.rows)
+          response.status(200).json(res.rows);
         }
         client.end();
     });
@@ -35,26 +48,22 @@ const getStateMode = (request, response) => {
   client.query('SELECT MODE() WITHIN GROUP (ORDER BY state) AS state_mode FROM beatboxers;', function(err, res) {
     if (err) throw err;
       for (let row of res.rows) {
-        response.status(200).json(res.rows)
+        response.status(200).json(res.rows);
       }
       client.end();
   });
 }
-
+*/
 // Queries DB for all beatboxer info
-const getAllBeatboxers = (request, response) => {
-    client.query('SELECT * FROM beatboxers', function(err, res) {
-      if(err) {
-        return console.error('error running query', err);
-      }
-      response.status(200).json(res.rows)
-      client.end();
-    });
-  }
   
-// Exports all query results
-module.exports = {
-    getAllBeatboxers,
-    getStateMode,
-    getCityMode
+/*
+const getAllBeatboxers = (request, response) => {
+  client.query('SELECT * FROM beatboxers', function(err, res) {
+    if(err) {
+      return console.error('error running query', err);
+    }
+    response.status(200).json(res.rows);
+    client.end();
+  });
 }
+*/
