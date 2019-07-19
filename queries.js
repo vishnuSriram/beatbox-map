4bfed4c9161209e8e962d58f74b244d70e158560
Vishnu Sriram <vsriram42@gmail.com>
2019-07-18 20:07:22 -0500

    Dialect options instead of ssl options

--- queries.js
+++ queries.js
@@ -2,7 +2,9 @@ const { Client } = require('pg');
 
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
-  ssl: true,
+  dialectOption: {
+    ssl: true
+  }
 });
 
 client.connect();
