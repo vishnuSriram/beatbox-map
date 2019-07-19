289afdd17538ef049d2d3401bde782b18d9fa9b7
Vishnu Sriram <vsriram42@gmail.com>
2019-07-18 20:15:50 -0500

    Omit service worker code!

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import unregisterServiceWorker from './unregisterServiceWorker';
+// import unregisterServiceWorker from './unregisterServiceWorker';
 import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-unregisterServiceWorker();
+// unregisterServiceWorker();
