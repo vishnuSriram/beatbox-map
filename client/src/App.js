77a82d76337c7f80b9e277067cb415ae9f9fb941
Vishnu Sriram <vsriram42@gmail.com>
2019-08-01 16:41:34 -0500

    Added a lil' navbar

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,14 @@ class App extends React.Component {
    render() {
       return (
          <div className="App">
+            <div class="topnav">
+               <a class="active" href="home">Home</a>
+               <a href="search">Search Beatboxer</a>
+               <a href="add">Add Beatboxer</a>
+               <a href="update">Update Beatboxer</a>
+               <a href="delete">Delete Beatboxer</a>
+            </div>
+
             <h1>Welcome to Beatbox Map!</h1>
             <p>Written by: Vishnu, Shak, and Abdulla</p>
             <p>Here's a list of all the beatboxers in the Beatbox Map database!</p>
